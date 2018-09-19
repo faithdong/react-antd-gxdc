8cbdcbf69f01dcec6799fd6186545db17dfeadc7
zhongxiaodong <zxdzyadmin@yonyou.com>
2018-09-19 18:02:31 +0800

    ddd

---
+++ src/pages/city/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: zhongxd 
+ * @Date: 2018-09-19 17:02:18 
+ * @Last Modified by: zhongxd
+ * @Last Modified time: 2018-09-19 17:02:46
+ */
+
+
+ import React from 'react';
+ 
--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,3 +1,4 @@
+
 /*
  * @Author: zhongxd 
  * @Date: 2018-09-18 10:11:41 
@@ -6,13 +7,19 @@
  */
 
 import React from 'react';
-import { Card, Table } from 'antd';
-import axios from 'axios';
+import { Card, Table , Modal , Button , message } from 'antd';
+import axios from '../../axios/index';
+import Utils from './../../utils/utils';
 
 export default class BasicTable extends React.Component {
- 
+
   state = {
-    dataSource2:[]
+    dataSource: [],
+    dataSource2: []
+  };
+
+  params = {
+    page:1
   };
 
   componentWillMount() {
@@ -47,21 +54,66 @@ export default class BasicTable extends React.Component {
         address: '北京市海淀区奥林匹克公园',
         time: '09:00'
       },
-    ]
+    ];
+    //给data添加key
+    data.map((item,index)=>{
+      item.key = index;
+    })
     this.setState({ dataSource: data });
     this.request();
   }
 
+  onRowClick=(recode,index)=>{
+     let selectKey = [index];
+     Modal.info({
+       title:'信息',
+       content:`用户名 ${recode.userName}`
+     })
+     this.setState({selectedRowKeys:selectKey,selecteItem:recode});
+  }
 
-  request = () =>{
-    let baseApi = 'https://www.easy-mock.com/mock/5a7278e28d0c633b9c4adbd7/api';
-    axios.get(baseApi+'/table/list').then((res)=>{
-      //console.log(JSON.stringify(res));
-      if(res.status === 200 && res.data.code === 0){
-        this.setState({dataSource2:res.data.result.list});
+
+  request = () => {
+    let _this = this; 
+    axios.ajax({
+      url: '/table/list',
+      data: {
+        params: {
+          page: this.params.page
+        },
+      }
+    }).then((res) => {
+      if (res.code == 0) {
+        res.result.list.map((item,index)=>{
+          item.key = index;
+        })
+        this.setState({ 
+          dataSource2: res.result.list,
+          selectedRowKeys:[],
+          selectedRows:null, 
+          pagination:Utils.pagination(res,(current)=>{
+            this.params.page = current;
+            this.request();
+          })
+        });
       }
     })
   }
+
+  handleDelete=(()=>{
+    let rows = this.state.selectedRows;
+    let ids = [];
+    rows.map((item)=>{
+      ids.push(item.id);
+    });
+    Modal.confirm({
+      title:'删除提示',
+      content:`您确定要删除这些数据 ${ids.join(',')}`,
+      onOk:()=>{
+        message.success('删除成功');
+      }
+    })
+  })
 
   render() {
     const columns = [
@@ -79,19 +131,44 @@ export default class BasicTable extends React.Component {
         title: '性别',
         key: 'sex',
         dataIndex: 'sex',
+        render(sex){
+          return sex==1?"男":"女";
+        }
 
       },
       {
         title: '状态',
         key: 'state',
         dataIndex: 'state',
+        render(state){
+          let config = {
+            '1':'咸鱼一条',
+            '2':'风华浪子',
+            '3':'北大才子',
+            '4':'百度FE',
+            '5':'创业者'
+          };
+          return config[state];
+        }
 
       },
       {
         title: '爱好',
         key: 'interest',
         dataIndex: 'interest',
-
+        render(abc) {
+          let config = {
+              '1': '游泳',
+              '2': '打篮球',
+              '3': '踢足球',
+              '4': '跑步',
+              '5': '爬山',
+              '6': '骑行',
+              '7': '桌球',
+              '8': '麦霸'
+          };
+          return config[abc];
+        }
       },
       {
         title: '生日',
@@ -108,16 +185,64 @@ export default class BasicTable extends React.Component {
         key: 'time',
         dataIndex: 'time'
       }
-    ]
-
+    ];
+    
+    const selectedRowKeys = this.state.selectedRowKeys;
+    const rowSelection = {
+      selectedRowKeys,
+      type:"radio"
+    };
+    const rowCheckSelection = {
+      type:"checkbox",
+      selectedRowKeys,
+      onChange:(selectedRowKeys,selectedRows)=>{
+        this.setState({
+            selectedRowKeys,
+            selectedRows
+        })
+    }
+    };
     return (
       <div>
         <Card title="基础表格" className="card-wrap">
           <Table dataSource={this.state.dataSource} columns={columns}></Table>
-        </Card> 
+        </Card>
 
-        <Card title="动态渲染表格" style={{ marginTop: 10 }}>
+        <Card title="动态渲染表格-mock" style={{ marginTop: 10 }}>
           <Table columns={columns} dataSource={this.state.dataSource2}></Table>
+        </Card>
+
+        <Card title="mock-单选表格" style={{ marginTop: 10 }}>
+          <Table  rowSelection={rowSelection} 
+            columns={columns} 
+            dataSource={this.state.dataSource2}
+            onRow={ (record,index) =>{
+              return {
+                onClick:()=>{ //点击行
+                  this.onRowClick(record,index);
+                }
+              }  
+            }}>
+          </Table>
+        </Card>
+
+        <Card title="mock-复选表格" style={{ marginTop: 10 }}>
+          <div style={{ marginBottom: 10 }}>
+            <Button type="primary" onClick={this.handleDelete}>删除</Button>
+          </div>
+          <Table  rowSelection={rowCheckSelection} 
+            columns={columns} 
+            dataSource={this.state.dataSource2}
+            >
+          </Table>
+        </Card>
+
+        <Card title="mock-表格分页" style={{ marginTop: 10 }}>
+          <Table  columns={columns} 
+            dataSource={this.state.dataSource2}
+            pagination={this.state.pagination}
+            >
+          </Table>
         </Card>
       </div>
     )
